628352247fd5ef325dc4af0d90b6838a5d9c458c
Dheeraj raju <dheshal07@gmail.com>
2025-11-09 18:56:08 +0530

    student record and sort the record

--- Assignment/Program_2.cpp
+++ Assignment/Program_2.cpp
@@ -1,55 +1,45 @@
 //write a c++ program to define a class called student with datamember as roll number, name and marks, define the appropriate member function 
 //1.setdata 2.display for reading and displaying the data and also define a member function called sort_data() to sort 5 students resukts according to rollnumber, name and marks
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
 class Student {
 public:
-    int roll_no;
+    int roll;
     string name;
     float marks;
 
     void setData() {
-        cout << "Enter Roll Number: ";
-        cin >> roll_no;
-        cout << "Enter Name: ";
-        cin >> name;
-        cout << "Enter Marks: ";
-        cin >> marks;
+        cout << "Enter Roll No, Name and Marks: ";
+        cin >> roll >> name >> marks;
     }
 
     void display() {
-        cout << roll_no << "\t" << name << "\t" << marks << endl;
+        cout << roll << "\t" << name << "\t" << marks << endl;
     }
 
-    // Static function to sort array of students
-    static void sort_data(Student s[], int n) {
+    void sort_data(Student s[], int n) {
         int choice;
-        cout << "\nSort by: \n1. Roll Number \n2. Name \n3. Marks\nEnter choice: ";
+        cout << "\nSort by:\n1. Roll Number\n2. Name\n3. Marks\nEnter your choice: ";
         cin >> choice;
 
-        switch(choice) {
-            case 1:
-                sort(s, s + n, [](Student a, Student b) {
-                    return a.roll_no < b.roll_no;
-                });
-                break;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                bool swap_needed = false;
 
-            case 2:
-                sort(s, s + n, [](Student a, Student b) {
-                    return a.name < b.name;
-                });
-                break;
+                if (choice == 1 && s[i].roll > s[j].roll)
+                    swap_needed = true;
+                else if (choice == 2 && s[i].name > s[j].name)
+                    swap_needed = true;
+                else if (choice == 3 && s[i].marks < s[j].marks)
+                    swap_needed = true;
 
-            case 3:
-                sort(s, s + n, [](Student a, Student b) {
-                    return a.marks > b.marks;  // Higher marks first
-                });
-                break;
-
-            default:
-                cout << "Invalid choice!" << endl;
+                if (swap_needed) {
+                    Student temp = s[i];
+                    s[i] = s[j];
+                    s[j] = temp;
+                }
+            }
         }
     }
 };
@@ -57,21 +47,16 @@ public:
 int main() {
     Student s[5];
 
-    // Input student data
-    for(int i = 0; i < 5; i++) {
-        cout << "\nEnter details for Student " << i + 1 << ":\n";
+    cout << "Enter details of 5 students:\n";
+    for (int i = 0; i < 5; i++)
         s[i].setData();
-    }
 
-    // Sorting
-    Student::sort_data(s, 5);
+    s[0].sort_data(s, 5);
 
-    // Display after sorting
-    cout << "\nRoll No\tName\tMarks\n";
-    for(int i = 0; i < 5; i++) {
+    cout << "\nSorted Student Details:\n";
+    cout << "Roll\tName\tMarks\n";
+    for (int i = 0; i < 5; i++)
         s[i].display();
-    }
 
     return 0;
 }
-
